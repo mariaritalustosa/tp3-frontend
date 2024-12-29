336055afee810f71b6fdb68865e8367c2d7912ef
Maria Rita <maria@mariarita.lustosa>
2024-12-29 12:31:04 -0300

    estilização da página inical do Republikanos

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
@@ -11,7 +12,7 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Bem-vindo ao Republikanos</h1>} />
+        <Route path="/" element={<h1 className="centered-heading">Bem-vindo ao Republikanos</h1>} />
         <Route path="/anuncios" element={<Anuncios />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,5 +2,5 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 
-const root = createRoot(document.getElementById('root')); // Verifique se o ID "root" está correto no HTML
+const root = createRoot(document.getElementById('root')); 
 root.render(<App />);
